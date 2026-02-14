357e07c8939535973d9c76565aa9c0a980e8b743
Fariha Urmi <farihaurmi@Farihas-MacBook-Air.local>
2026-02-14 22:43:51 +0600

    add origin

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['https://blood-donation-11.web.app', 'http://localhost:5173'],
     credentials: true,
   })
 );
